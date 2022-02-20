40cf5c7fd611e957c60fbb8f03793071fc87a220
gkatzioura <gkatzioura@gmail.com>
2022-02-20 11:55:54 +0000

    Switched to Redis persistence

--- Chapter2/go.mod
+++ Chapter2/go.mod
@@ -2,4 +2,13 @@ module task_manager
 
 go 1.16
 
-require github.com/gin-gonic/gin v1.7.7 // indirect
+require (
+	github.com/gin-gonic/gin v1.7.7
+	github.com/go-redis/redis/v8 v8.11.4 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/go-cmp v0.5.6 // indirect
+	github.com/stretchr/testify v1.5.1 // indirect
+	golang.org/x/sys v0.0.0-20210423082822-04245dca01da // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
--- Chapter2/main.go
+++ Chapter2/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis/v8"
 )
 
 type Task struct {
@@ -13,7 +17,14 @@ type Task struct {
 	Timestamp   int64  `json:"timestamp"`
 }
 
-var taskMap = make(map[string]Task)
+var (
+	client = redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+	taskMap = make(map[string]Task)
+)
 
 func setupRouter() *gin.Engine {
 	r := gin.Default()
@@ -25,22 +36,24 @@ func setupRouter() *gin.Engine {
 
 	// Get tasks
 	r.GET("/task", func(c *gin.Context) {
-		tasks := []Task{}
-		for _, v := range taskMap {
-			tasks = append(tasks, v)
+		if tasks, err := fetchTasks(c); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
+		} else {
+			c.JSON(http.StatusOK, gin.H{"tasks": tasks})
 		}
 
-		c.JSON(http.StatusOK, gin.H{"tasks": tasks})
 	})
 
 	// Get task
 	r.GET("/task/:id", func(c *gin.Context) {
 		id := c.Params.ByName("id")
-		task, ok := taskMap[id]
-		if ok {
-			c.JSON(http.StatusOK, gin.H{"task": task})
-		} else {
+
+		if task, err := fetchTask(c.Request.Context(), id); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"id": id, "message": err.Error()})
+		} else if task == nil {
 			c.JSON(http.StatusNotFound, gin.H{"id": id, "message": "not found"})
+		} else {
+			c.JSON(http.StatusOK, gin.H{"task": task})
 		}
 	})
 
@@ -49,21 +62,106 @@ func setupRouter() *gin.Engine {
 		var task Task
 
 		if err := c.BindJSON(&task); err != nil {
-			c.JSON(http.StatusOK, gin.H{"task": task, "created": false, "message": err.Error()})
-		} else {
-			taskMap[task.Id] = task
-			c.JSON(http.StatusCreated, gin.H{"task": task, "created": true, "message": "Task Created Successfully"})
+			c.JSON(http.StatusInternalServerError, gin.H{"task": task, "created": false, "message": err.Error()})
+			return
 		}
 
+		if err := persistTask(c, task); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"task": task, "created": false, "message": err.Error()})
+			return
+		}
+
+		c.JSON(http.StatusCreated, gin.H{"task": task, "created": true, "message": "Task Created Successfully"})
 	})
 
 	r.DELETE("/task/:id", func(c *gin.Context) {
 		id := c.Params.ByName("id")
-		delete(taskMap, id)
-		c.JSON(http.StatusOK, gin.H{"id": id, "message": "deleted"})
+		if err := deleteTask(c.Request.Context(), id); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"id": id, "message": err.Error()})
+		} else {
+			c.JSON(http.StatusOK, gin.H{"id": id, "message": "Task deleted"})
+		}
+
 	})
 
 	return r
+}
+
+func persistTask(c context.Context, task Task) error {
+	hmset := client.HSet(c, fmt.Sprintf("task:%s", task.Id), "Id", task.Id, "Name", task.Name, "Description", task.Description, "Timestamp", task.Timestamp)
+
+	if hmset.Err() != nil {
+		return hmset.Err()
+	}
+
+	z := redis.Z{Score: float64(task.Timestamp), Member: task.Id}
+	zadd := client.ZAdd(c, "tasks", &z)
+
+	if zadd.Err() != nil {
+		return hmset.Err()
+	}
+
+	return nil
+}
+
+func fetchTask(c context.Context, id string) (*Task, error) {
+	hgetAll := client.HGetAll(c, fmt.Sprintf("task:%s", id))
+
+	if err := hgetAll.Err(); err != nil {
+		return nil, err
+	}
+
+	ires, err := hgetAll.Result()
+
+	if err != nil {
+		return nil, err
+	}
+
+	if l := len(ires); l == 0 {
+		return nil, nil
+	}
+
+	timestamp, _ := strconv.ParseInt(ires["Timestamp"], 10, 64)
+	task := Task{Id: ires["Id"], Name: ires["Name"], Description: ires["Description"], Timestamp: timestamp}
+	return &task, nil
+}
+
+func deleteTask(c context.Context, id string) error {
+	if err := client.Unlink(c, fmt.Sprintf("task:%s", id)).Err(); err != nil {
+		return err
+	}
+
+	if err := client.ZRem(c, "tasks", id).Err(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func fetchTasks(c context.Context) ([]*Task, error) {
+	var tasks []*Task
+
+	zRange := client.ZRange(c, "tasks", 0, -1)
+
+	if err := zRange.Err(); err != nil {
+		return nil, err
+	}
+
+	ids, err := zRange.Result()
+
+	if err != nil {
+		return nil, err
+	}
+
+	for _, id := range ids {
+		if task, err := fetchTask(c, id); err != nil {
+			return nil, err
+		} else {
+			tasks = append(tasks, task)
+		}
+	}
+
+	return tasks, nil
 }
 
 func main() {
