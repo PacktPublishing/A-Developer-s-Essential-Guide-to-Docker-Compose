2f10b0acffaa4f536f20f7b3044178247503ed70
gkatzioura <gkatzioura@gmail.com>
2022-06-02 21:44:07 +0100

    Added go source code

---
+++ Chapter8/newsletter-lambda/go.mod
@@ -0,0 +1,10 @@
+module newsletter-lambda
+
+go 1.18
+
+require (
+	github.com/aws/aws-lambda-go v1.32.0
+	github.com/aws/aws-sdk-go v1.44.26
+)
+
+require github.com/jmespath/go-jmespath v0.4.0 // indirect
--- Chapter8/newsletter-lambda/newsletter.go
+++ Chapter8/newsletter-lambda/newsletter.go
@@ -4,13 +4,16 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/private/protocol/json/jsonutil"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+	"github.com/aws/aws-sdk-go/service/sqs"
 )
 
 type Subscribe struct {
@@ -39,21 +42,23 @@ func HandleRequest(ctx context.Context, subscribe Subscribe) (string, error) {
 
 		_, err = dynamoDb.PutItem(input)
 		if err != nil {
-			return "Could not add item",err
+			return "Could not add item", err
 		}
+
+		sendToSQS(subscribe)
 
 		return fmt.Sprintf("You have been subscribed to the %s newsletter", subscribe.Topic), nil
 	}
 }
 
-func dynamoDBSession() (*dynamodb.DynamoDB, error) {
-	session, err := createSession()
-
-	if err != nil {
-		return nil, err
+func isSimulated() bool {
+	if value := os.Getenv("SIMULATED"); len(value) == 0 {
+		return false
+	} else if value != "true" {
+		return false
 	}
 
-	return dynamodb.New(session, aws.NewConfig().WithEndpoint("http://host.docker.internal:8000").WithRegion("eu-west-2")), nil
+	return true
 }
 
 func createSession() (*session.Session, error) {
@@ -63,6 +68,53 @@ func createSession() (*session.Session, error) {
 	})
 }
 
+func dynamoDBSession() (*dynamodb.DynamoDB, error) {
+	session, err := createSession()
+
+	if err != nil {
+		return nil, err
+	}
+
+	if isSimulated() {
+		return dynamodb.New(session, aws.NewConfig().WithEndpoint(os.Getenv("DYNAMODB_ENDPOINT")).WithRegion("eu-west-2")), nil
+	} else {
+		return dynamodb.New(session), nil
+	}
+}
+
+func sendToSQS(subscribe Subscribe) {
+	if !isSimulated() {
+		return
+	}
+
+	if session, err := sqsSession(); err == nil {
+		if bytes, err := jsonutil.BuildJSON(subscribe); err == nil {
+			smsInput := &sqs.SendMessageInput{
+				MessageBody: aws.String(string(bytes)),
+				QueueUrl:    aws.String(os.Getenv("SQS_TOPIC")),
+			}
+
+			if _, err := session.SendMessage(smsInput); err != nil {
+				fmt.Println(err)
+			}
+
+		} else {
+			fmt.Println(err.Error())
+		}
+	} else {
+		fmt.Println(err.Error())
+	}
+}
+
+func sqsSession() (*sqs.SQS, error) {
+	session, err := createSession()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return sqs.New(session, aws.NewConfig().WithEndpoint(os.Getenv("SQS_ENDPOINT")).WithRegion("eu-west-2")), nil
+}
 func main() {
 	lambda.Start(HandleRequest)
 }
