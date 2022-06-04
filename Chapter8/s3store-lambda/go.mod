429c28b065b658d9903eb5d694a90ea237afd0ef
gkatzioura <gkatzioura@gmail.com>
2022-06-04 10:47:19 +0100

    Added lambda for s3

---
+++ Chapter8/s3store-lambda/go.mod
@@ -0,0 +1,11 @@
+module s3store-lambda
+
+go 1.18
+
+require github.com/aws/aws-lambda-go v1.32.0
+
+require (
+	github.com/aws/aws-sdk-go v1.44.27 // indirect
+	github.com/aws/aws-sdk-go-v2 v1.16.4 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+)
---
+++ Chapter8/s3store-lambda/s3store.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"crypto/md5"
+	"encoding/json"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+)
+
+const (
+	SUBSCRIPTION_BUCKET_ENV = "SUBSCRIPTION_BUCKET"
+	SIMULATED_ENV           = "SIMULATED"
+	S3_ENDPOINT_ENV         = "S3_ENDPOINT"
+	AWS_REGION_ENV          = "AWS_DEFAULT_REGION"
+)
+
+type Subscribe struct {
+	Email string `json:"email"`
+	Topic string `json:"topic"`
+}
+
+func HandleRequest(ctx context.Context, sqsEvent events.SQSEvent) error {
+	session := s3Session()
+
+	for _, message := range sqsEvent.Records {
+		var subscribe Subscribe
+		json.Unmarshal([]byte(message.Body), &subscribe)
+
+		key := fmt.Sprintf("%s.%d", hash(subscribe.Email), time.Now().UnixNano()/int64(time.Millisecond))
+
+		marshalled, _ := json.Marshal(subscribe)
+
+		session.PutObject(&s3.PutObjectInput{
+			Bucket: aws.String(os.Getenv(SUBSCRIPTION_BUCKET_ENV)),
+			Key:    aws.String(key),
+			Body:   bytes.NewReader(marshalled),
+		})
+	}
+
+	return nil
+}
+
+func hash(email string) string {
+	data := []byte(email)
+	return fmt.Sprintf("%x", md5.Sum(data))
+}
+
+func isSimulated() bool {
+	if value := os.Getenv(SIMULATED_ENV); len(value) == 0 {
+		return false
+	} else if value != "true" {
+		return false
+	}
+
+	return true
+}
+
+func s3Session() *s3.S3 {
+	session, _ := session.NewSession()
+
+	if isSimulated() {
+		return s3.New(session, aws.NewConfig().WithEndpoint(os.Getenv(S3_ENDPOINT_ENV)).WithRegion(os.Getenv(AWS_REGION_ENV)))
+	} else {
+		return s3.New(session, aws.NewConfig().WithRegion(os.Getenv(AWS_REGION_ENV)))
+	}
+}
+
+func main() {
+	lambda.Start(HandleRequest)
+}
