8cc5454e9876cbcd80b87b927f5da8cc6c31fb25
gkatzioura <gkatzioura@gmail.com>
2022-06-26 11:32:55 +0100

    Added ecr

--- Chapter11/aws/main.tf
+++ Chapter11/aws/main.tf
@@ -17,3 +17,13 @@ terraform {
 provider "aws" {
   region  = "eu-west-1"
 }
+
+resource "aws_ecr_repository" "foo" {
+  name                 = "developer-guide-to-compose-ecr"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
